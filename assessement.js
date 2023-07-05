9e3d92f88ce8ae58c901d9e67f51c7b8853d7fc3
orishaba-mercy <orishabamercybanga@gmail.com>
2023-07-05 23:52:47 +0300

    changes

--- assessement.js
+++ assessement.js
@@ -6,144 +6,374 @@
 // objects, and how inheritance might come into play if there are different types of
 // stories or storytellers.
 
-class culturesStory{
-    constructor(length,moralLessons,age_group){
-       this.length= length
-       this.moralLessons=moralLessons
-       this.age_group=age_group
-    }
+// class Story{
+//     constructor(length,moralLessons,age_group){
+//        this.length= length
+//        this.moralLessons=moralLessons
+//        this.age_group=age_group
+//     }
     
-
-}
-class StoryTeller{
-    constructor(name){
-        this.name=name
+// }
+// class StoryTeller{
+//     constructor(name){
+//         this.name=name
        
-    }
+//     }
     
-    Teller(story){
+//     Teller(story){
 
         
-    }
-}
+//     }
+// }
 
-class oldStoryTeller  extends StoryTeller{
-    constructor(name,story){
-        super(name);
-        this.story=story
+// class oldStoryTeller  extends Story{
+//     constructor(name,story){
+//         super(name);
+//         this.story=story
 
-    }
-}
-class YouthStoryTeller extends StoryTeller{
-    constructor(name,story){
-        this.story=story
-    super(name);   
-    }
+//     }
+// }
+// class YouthStoryTeller extends Story{
+//     constructor(name,story){
+//         this.story=story
+//     super(name);   
+//     }
+// }
+// class Translator extends StoryTeller{
+//     constructor(differentlanguages){
+//         this.differentlanguages
+//     }
 
-   
-}
-class Translator extends StoryTeller{
-    constructor(differentlanguages){
-        this.differentlanguages
-    }
-
-}  
+// }  
 
 
-const story = new story("The Lion and the Hare", "Medium", "Never underestimate your opponent.", "Children");
+// const story= new Story("The Lion and the Hare", "Medium", "Never underestimate your opponent.", "Children");
 
 
-const oldStoryTeller = new oldStoryTeller("orishaba", 20);
+// const oldStoryTeller = new oldStoryTeller("orishaba", 20);
 
 
-const YouthStoryTeller = new YouthStoryTeller("Kwash hope", "12");
+// const YouthStoryTeller = new YouthStoryTeller("Kwash hope", "12");
 
 
-const translator = new Translator();
-oldStoryTeller.tellStory(story);
-const Translator = translator.translate(story, "Germeny",story,'kiswahili');
-console.log(Translator);
+// const translator = new Translator();
+// oldStoryTeller.tellStory(story);
+// const translator = translator.translate(story, "Germeny",story,'kiswahili');
+// console.log(Translator);
 
 
 
-// **African Cuisine:** You're creating a recipe app specifically for African cuisine.
-// The app needs to handle recipes from different African countries, each with its
-// unique ingredients, preparation time, cooking method, and nutritional
-// information. Consider creating a `Recipe` class, and think about how you might
-// create subclasses for different types of recipes (e.g., `MoroccanRecipe`,
-// `EthiopianRecipe`, `NigerianRecipe`), each with their own unique properties and
-// methods.
-class Recipe {
-    constructor(name, country, ingredients, Time, cookingMethod, nutritionalInfo) {
-      this.name = name;
-      this.country = country;
-      this.ingredients = ingredients;
-      this.Time = Time;
-      this.cookingMethod = cookingMethod;
-      this.nutritionalInfo = nutritionalInfo;
-    }
-  
-    displayRecipe() {
-      console.log(nageria);
-      console.log(uganda);
-      console.log();
-      console.log(6);
-      console.log(mixtures);
-      console.log(reach in vitamins);
-    }
-  }
-  
-  class MoroccanRecipe extends Recipe {
-    constructor(name, ingredients, preparationTime, cookingMethod, nutritionalInfo, spices) {
-      super(name, "Morocco", ingredients, preparationTime, cookingMethod, nutritionalInfo);
-      this.spices = spices;
-    }
-  
-    displayRecipe() {
-      super.displayRecipe();
-      console.log(`Spices: ${this.spiceLevel}`);
-    }
-  }
-  
-  class EthiopianRecipe extends Recipe {
-    constructor(name, ingredients, preparationTime, cookingMethod, nutritionalInfo, injeraIncluded) {
-      super(name, "Ethiopia", ingredients, preparationTime, cookingMethod, nutritionalInfo);
-      this.mistMeat = mistMeat;
-    }
-  
-    displayRecipe() {
-      super.displayRecipe();
-      console.log(`mistMeat: ${this.mistMeat}`);
-    }
-  }
-  
-  class NigerianRecipe extends Recipe {
-    constructor(name, ingredients, preparationTime, cookingMethod, nutritionalInfo, Spicy) {
-      super(name, "Nigeria", ingredients, preparationTime, cookingMethod, nutritionalInfo);
-      this.Spicy = Spicy;
-    }
-  
-    displayRecipe() {
-      super.displayRecipe();
-      console.log(`Spicy: ${this.Spicy}`);
-    }
-  }
-  
-  
-  const moroccanRecipe = new MoroccanRecipe(
-    "Moroccan Couscous",
-    ["Couscous", "Vegetables", "Meat", "Spices"],
-    "1 hour",
-    "Steam",
-    "Calories: 300",
-    "Medium"
-  );
-  moroccanRecipe.displayRecipe();
-  
   
  
   
   
   
+// class StoryTeller {
+//   constructor(name, language, age_group, title) {
+//       this.name = name;
+//       this.language = language;
+//       this.age_group = age_group;
+//       this.title = title;
+//   }
+// }
+
+// class Translator extends StoryTeller {
+//   constructor(name, language, age_group, title) {
+//       super(name, language, age_group, title);
+//   }
+
+//   translate_story(story, target_language) {
+//       console.log(`Translating story into ${target_language}...`);
+
+//       console.log("Translated story:");
+//       console.log(`Content: ${story.content}`);
+//       console.log(`Author: ${story.author}`);
+//       console.log(`Moral Lessons: ${story.moral_lessons}`);
+//       console.log(`Age Group: ${story.age_group}`);
+//       console.log(`Length: ${story.length}`);
+//       console.log(`Title: ${story.title}`);
+//   } 
+// }
+// class Story {
+//   constructor(content, author, moral_lessons, age_group, length, title) {
+//       this.content = content;
+//       this.author = author;
+//       this.moral_lessons = moral_lessons;
+//       this.age_group = age_group;
+//       this.length = length;
+//       this.title = title;
+//   }
+// }
+
+
+
+// let story = new Story("Once upon a time...", "Bridget", "Always be kind", "Children", 10, "The River Between");
+// let storyteller = new StoryTeller("Janey", "English", "Children", "Storyteller");
+// let sranslator = new Translator("Alexin", "French", "Children", "Translator");
+
+// Translator.translate_story(story, "French");
+
   
+
+
+// // // **African Cuisine:** You're creating a recipe app specifically for African cuisine.
+// // // The app needs to handle recipes from different African countries, each with its
+// // // unique ingredients, preparation time, cooking method, and nutritional
+// // // information. Consider creating a `Recipe` class, and think about how you might
+// // // create subclasses for different types of recipes (e.g., `MoroccanRecipe`,
+// // // `EthiopianRecipe`, `NigerianRecipe`), each with their own unique properties and
+// // methods.
+
+class Recipe {
+  constructor(name, ingredients, preparationTime, cookingMethod, nutritionalInfo) {
+	this.name = name;this.ingredients = ingredients;
+	this.preparationTime = preparationTime;
+	this.cookingMethod = cookingMethod;
+	this.nutritionalInfo = nutritionalInfo;
+  }
+
+  displayRecipe() {
+	console.log(`Recipe: ${this.name}`);
+	console.log(`Ingredients: ${this.ingredients}`);
+	console.log(`Preparation Time: ${this.preparationTime}`);
+	console.log(`Cooking Method: ${this.cookingMethod}`);
+	console.log(`Nutritional Information: ${this.nutritionalInfo}`);
+  }
+}
+
+class MoroccanRecipe extends Recipe {
+  constructor(name, ingredients, preparationTime, cookingMethod, nutritionalInfo, spiceLevel) {
+	super(name, ingredients, preparationTime, cookingMethod, nutritionalInfo);
+	this.spiceLevel = spiceLevel;
+  }
+
+  displayRecipe() {
+	super.displayRecipe();
+	console.log(`Spice Level: ${this.spiceLevel}`);
+  }
+}
+
+class EthiopianRecipe extends Recipe {
+  constructor(name, ingredients, preparationTime, cookingMethod, nutritionalInfo, injeraIncluded) {
+	super(name, ingredients, preparationTime, cookingMethod, nutritionalInfo);
+	this.injeraIncluded = injeraIncluded;
+  }
+
+  displayRecipe() {
+	super.displayRecipe();
+	console.log(`Injera Included: ${this.injeraIncluded}`);
+  }
+}
+
+
+class NigerianRecipe extends Recipe {
+  constructor(name, ingredients, preparationTime, cookingMethod, nutritionalInfo, jollofRiceIncluded) {
+	super(name, ingredients, preparationTime, cookingMethod, nutritionalInfo);
+	this.jollofRiceIncluded = jollofRiceIncluded;
+  }
+
+  displayRecipe() {
+	super.displayRecipe();
+	console.log(`Jollof Rice Included: ${this.jollofRiceIncluded}`);
+  }
+}
+
+
+const moroccanRecipe = new MoroccanRecipe( 'Moroccan maize floor',
+  ['oil', 'greens', 'Carrots', 'Tomatoes', 'Spices'],'2 hours', 'Well cooked and ready to be eaten','Calories, Fat, Protein', 'small'
+ 
+);
+moroccanRecipe.displayRecipe();
+
+const ethiopianRecipe = new EthiopianRecipe( 'shabwe', ['Chicken', 'Onions', 'Greens', 'Ginger'],
+'1.5 hours','Simmering in a spicy sauce only the spices and others', ' Fat: 5g, Protein: 15g',)
+ 
+ 
+ethiopianRecipe.displayRecipe();
+
+const nigerianRecipe = new NigerianRecipe( 'Jollof Rice', ['Rice', 'Tomatoes', 'Onions', 'Peppers', 'Chicken'],
+'45 minutes','One-pot cooking', 'Calories: 350, Fat: 10g, Protein: 20g')
+ 
+nigerianRecipe.displayRecipe();
+
+
+// **Wildlife Preservation:** You're a wildlife conservationist working on a
+// program to track different species in a national park. Each species has its own
+// characteristics and behaviors, such as its diet, typical lifespan, migration
+// patterns, etc. Some species might be predators, others prey. You'll need to
+
+// create classes to model `Species`, `Predator`, `Prey`, etc., and think about how
+// these classes might relate to each other through inheritance.
+
+
+class Spicies{
+  constructor(diet,lifespan,migrationPatters,name){
+    this.lifespan=lifespan
+    this.migrationPatters=migrationPatters
+    this.diet=diet
+    this.name=name
+  }
+  getDiscription(){
+    return `${this.name} feeds on ${this.diet} and it stays for ${this.lifespan}`;
+  }
+  getDiet(){
+    return` ${this.name} mostly feed on ${this.diet}`
+  }
+  getmigrationPatterns(){
+    return`Gazelles move every ${this.migrationPatters} look for water and pasture`;
+  }
+}
+
+class Predator extends Spicies{
+  constructor(name,lifespan,migrationPatters,diet,huntaingMethods){
+    super(name,diet,lifespan,migrationPatters)
+    this.huntaingMethods=huntaingMethods
+  }
+  getDiscription(){
+    return ` ${super.getDiscription()} its a Predator that moves in groups when migrating ${this.migrationPatters}`;
+  }
+
+}
+class pray extends Spicies{
+  constructor(name,lifespan,migrationPatters,diet,predeterthreat){
+    super(name,diet,migrationPatters,lifespan)
+    this.predeterthreat=predeterthreat
+
+  }
+  getDiscription(){
+    return`${this.getDiscription} this follows ${this.migrationPatters}.it faces a predators threat level of ${this.predeterthreat}.`;
+  }
   
+}
+const species=new Spicies("Lion","meat",30,"abrapt Attack");
+
+const Pray =new pray("Zebra","grass",20,"group migration");
+console.log(Pray.getDiscription());
+const predators=new Predator("Lion","meat",5,"ambush hunting")
+console.log(predators.getDiscription());
+
+
+
+
+
+//gggggg
+
+//for art
+class Artist {
+  constructor(name, country) {
+    this.name = name;
+    this.country = country;
+    
+  }
+
+  perform() {
+    console.log(`${this.name} from ${this.country} is performing.`);
+  }
+}
+
+class Musician extends Artist {
+  constructor(name, country, instrument) {
+    super(name, country);
+    this.instrument = instrument;
+  }
+
+  perform() {
+    console.log(`${this.name} from ${this.country} is performing with ${this.instrument}.`);
+  }
+}
+
+class Dancer extends Artist {
+  constructor(name, country, danceStyle) {
+    super(name, country);
+    this.danceStyle = danceStyle;
+  }
+
+  perform() {
+    console.log(`${this.name} from ${this.country} is performing a ${this.danceStyle} dance.`);
+  }
+}
+
+class Performance {
+  constructor(artist, startTime, endTime) {
+    this.artist = artist;
+    this.startTime = startTime;
+    this.endTime = endTime;
+  }
+
+  start() {
+    console.log(`Performance by ${this.artist.name} starts at ${this.startTime}.`);
+  }
+
+  end() {
+    console.log(`Performance by ${this.artist.name} ends at ${this.endTime}.`);
+  }
+}
+
+class Stage {
+  constructor(name) {
+    this.name = name;
+    this.performances = [];
+  }
+
+  addPerformance(performance) {
+    this.performances.push(performance);
+  }
+
+  showSchedule() {
+    console.log(`Schedule for ${this.name}:`);
+    this.performances.forEach((performance, index) => {
+      console.log(`Performance ${index + 1}: ${performance.artist.name}, Start: ${performance.startTime}, End: ${performance.endTime}`);
+    });
+  }
+}
+
+const artist1 = new Musician("sauit", "kenya", "saxophone");
+const artist2 = new Dancer("Kora", "Senegal", "traditional");
+const artist3 = new Musician("Angelique Kidjo", "Benin", "vocals");
+
+const performance1 = new Performance(artist1, "18:00", "19:00");
+const performance2 = new Performance(artist2, "19:30", "20:00");
+const performance3 = new Performance(artist3, "20:30", "21:30");
+
+const stage1 = new Stage("Main Stage");
+stage1.addPerformance(performance1);
+stage1.addPerformance(performance2);
+stage1.addPerformance(performance3);
+
+stage1.showSchedule();
+performance1.start();
+performance1.end();
+
+artist2.perform();
+
+
+
+class Products{
+  constructor(name,quatity,price){
+    this.name=name
+    this.quatity=quatity
+    this.price=price
+  }
+  getTotalValue(){
+    return this.price*this.quatity
+  }
+  
+}
+// const product1=new Product{}
+
+
+
+//qution7
+class FlightBooking{
+  constructor(Destination,flight,Date,passangerSit){
+    this.Destination=Destination
+    this.flight=[]
+    this.Date=Date
+    this.passangerSit=passangerSit
+  }
+  availabeFlight(){
+    for(flight in flights){
+      if()
+    } 
+  }
+}
--- assessement.py
+++ assessement.py
@@ -1,45 +1,124 @@
-class storyTeller:
-    def __init__(self,name) :
+# class Artist:
+#     def __init__(self,name,country,type_of_music):
+#       self.name=name
+#       self.country=country
+#       self.type_of_music=type_of_music
+     
+
+#     def perform(self):
+#        print (f"{self.name} from {self.country} playing {self.type_of_music} ")
+
+# class Musicians(Artist):
+#    def __init_(self,name,country,instrament) :
+#       super.__init__(name,country)
+#       self.instrament=instrament   
+
+# def perform(self):
+#         return (f"{self.name} from {self.country} using this {self.instrament}")   
+
+# class Dancers(Artist):
+#     def __init_(self,name,country,dancing_styles) :
+#         super().__init__(name,country)
+#         self.dancing_styles=dancing_styles
+
+
+
+class Artist:
+    def __init__(self, name, country, music_style, instrument):
+        self.name = name
+        self.country = country
+        self.music_style = music_style
+        self.instrument = instrument
+    def display_details(self):
+        return f'Name: {self.name}, Country: {self.country}, Music Style: {self.music_style}, Instrument: {self.instrument}'
+class Performance(Artist):
+    def __init__(self, name, country, music_style, instrument, end_time, start_time):
+        super().__init__(name, country, music_style, instrument)
+        self.end_time = end_time
+        self.start_time = start_time
+    def performance_duration(self):
+        return self.end_time - self.start_time
+class Stage(Artist):
+    def __init__(self, name,  music_style, instrument, location):
+        super().__init__(name, country, music_style, instrument)
+        self.location = location
+        self.schedule = []
+    def addingTask(self, task):
+        self.schedule.append(task)
+# Create an instance of the Artist class
+artist = Artist("Marisah", "USA", "dancing", "guiter")
+print(artist.display_details())
+# Create an instance of the Performance class
+performance = Performance("Jane", "UK", "Pop", "Vocals", 24, 12)
+print(performance.performance_duration())
+# Create an instance of the Stage class
+stage = Stage("Jesus", "Kenya", "rocking", "piano", "hall")
+stage.addingTask("dancing")
+print(stage.schedule)
+
+#     def perform(self):
+#         return (f"{self.name} from{self.country} with different{self.dancing_styles}")
+    
+# class perfomance: 
+#     def __init__(self,name,start_time,end_time):
+#         self.name=name
+#         self.start_time=start_time
+#         self.end_time=end_time
+
+#     def start_end_time(self):
+#         return (f"artist{self.name} his perfomance starts at {self.start_time} and it will end{self.end_time}")
+
+# Artist1=Musicians("mercy","uganda","afro_music")
+# dancers=Dancers("MERCY","nigeria","global",)
+
+
+
+# Create a class called Product with attributes for name, price, and quantity.
+# Implement a method to calculate the total value of the product (price * quantity).
+# Create multiple objects of the Product class and calculate their total values.
+
+class Products:
+    def __init__(self,name,price,quantity):
         self.name=name
+        self.price=price
+        self.quantity=quantity
 
-def storytime(self,story):
-    if story==short:
-        return  "the the story of {story}"
+    def total_value(self):
+        return self.quantity*self.price
+    
+Product1=Products("Beans",3000,30)
+product2=Products("oranges",600,10)
 
-class oldperson():
-    def __init__(self,name,age):
-        super().__init__(name)
+tatal_value1=Product1.total_value()
+tatal_value2=product2.total_value()
+
+
+print(f"the total value of{Product1.name} costs ${tatal_value1}")
+
+
+         
+         
+# question6
+class Students:
+    def __init__(self,name,age,grades):
+        self.name=name
         self.age=age
+        self.grades=grades
+
+    def  calculate_average_grade (self):
+              total_marks=sum(self.grades) 
+              return(total_marks/len(number_of_grades)) 
+    
 
 
-class youthTeller(storyTeller):
-    def __init__(self, name,expertise):
-        super().__init__(name)
-        self.expertise=expertise
-      
-class Translatorall:
-    def __init__(self) :
-        def translatorall(self.story,differntLanguages):
+    def students_details(self):
+        return (f"self.name" ,"Total_Marks")
+        
 
 
-Storytime = storytime("Smart money", "30", "mummies.", "10")
-
-
-Oldperson = oldperson("Kanyabulo", 20)
-
-
-YouthTeller = youthTeller("megani yoy", "19")
-
- 
-
-
-oldperson.tell_story(story)
-Translatorall = translator.translate(story, "KISWAHILI")
-print(Translatorall)
+    
+       
        
         
-        
 
-
-
-      
+ 
